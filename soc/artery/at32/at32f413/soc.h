8348fab609cd86e5cff3a703e1f5838711b678f8
Charlie Palmer <charleshgpalmer@gmail.com>
2024-07-27 10:58:30 +0100

    Add ArteryTek at32f413cb SoC (which uses ARM Cortex-M4 arch)
    The soc directory defines:
    * soc.yml which sets the SOC metadata (name, series, family, etc.).
    * soc.h which defines things required for drivers to use this SoC.
    * Kconfig.soc which defines configs associated with SOC metadata from
      soc.yml such as SOC_AT32F413CBT7 which can be used in the build system
      for things like pulling in the correct sources from the AT HAL module.
    * CMakeLists.txt which defines the base linker script plus any include
      files and sources to pull in when using this SoC.

---
+++ soc/artery/at32/at32f413/soc.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2024, Charlie Palmer
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _SOC_AT32F413_H_
+#define _SOC_AT32F413_H_
+
+#ifndef _ASMLANGUAGE
+
+#include <at32f413.h>
+
+#endif /* _ASMLANGUAGE */
+
+#endif /* _SOC_AT32F413_H_ */
